2ca65423b3d8820659a4c0fdfff447a65f8d3aa1
Gabriel Mendes <gabriel.mendespng@gmail.com>
2021-04-29 00:23:09 -0300

    altera layout da tela de login e adiciona botão para logar com google

--- lib/Pages/Login/LoginPage.dart
+++ lib/Pages/Login/LoginPage.dart
@@ -1,5 +1,9 @@
 import 'package:animazing/Models/User.dart';
+import 'package:animazing/Pages/Login/components/Authentication.dart';
+import 'package:animazing/Pages/Login/components/SignInWithGoogleButton.dart';
+import 'package:animazing/Pages/Login/components/CreateAccountButton.dart';
 import 'package:animazing/Services/UserService.dart';
+import 'package:animazing/widgets/ScreenTitle.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,25 +11,73 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-          appBar: AppBar(
-            title: Text('Animazing'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Welcome'),
-                ElevatedButton(
-                    child: Text('LOGIN'),
-                    onPressed: () {
-                      User user = userService.getOne("test-user");
-                      Navigator.of(context).pushNamed('/app', arguments: user);
-                    })
+    return Scaffold(
+      body: Container(
+        decoration: new BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment(-4.0, -8.0),
+              end: Alignment(3.0, 6.0),
+              stops: [
+                0.2,
+                0.75,
               ],
-            ),
-          )),
+              colors: [
+                Color(0xfff1dbdb),
+                Color(0xfffaf7f7),
+              ]),
+        ),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(20, 40, 20, 50),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ScreenTitle(text: "Animazing"),
+                    ScreenSubTitle(
+                      text: "Torne o carinho com seus pets ainda mais incrível",
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 80, 0, 0),
+                      child: Image.network(
+                        "https://i.pinimg.com/originals/9f/13/ae/9f13ae857448aad272655220144012e3.png",
+                        width: 300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: FutureBuilder(
+                    future: Authentication.initializeFirebase(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Text("Ocorreu um erro ao inicializar Firebase");
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.done) {
+                        return Column(
+                          children: [
+                            SignInWithGoogleButton(),
+                            CreateAccountButton(),
+                          ],
+                        );
+                      }
+                      return CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                          Colors.orange,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

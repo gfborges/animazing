4a0301c21423bca5d0e91b02109bba908a45d910
Isabelle Oliveira <foliveira.isa@gmail.com>
2021-05-16 22:35:09 -0300

    localizacao atual

--- lib/Pages/MapPage.dart
+++ lib/Pages/MapPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapsPage extends StatefulWidget {
   @override
@@ -9,6 +10,20 @@ class MapsPage extends StatefulWidget {
 class _MapsPageState extends State<MapsPage> {
   // TODO: usar Places API
   GoogleMapController googleMapsController;
+  LatLng initialcameraposition = LatLng(-1.4437121, -48.4630365);
+  Location location = Location();
+
+  void _onMapCreated(GoogleMapController _cntlr)
+  {
+    googleMapsController = _cntlr;
+    location.onLocationChanged.listen((l) { 
+      googleMapsController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(l.latitude, l.longitude),zoom: 15),
+          ),
+      );
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -16,13 +31,16 @@ class _MapsPageState extends State<MapsPage> {
       height: 500,
       width: 500,
       child: Center(
-        child: GoogleMap(
+        child: GoogleMap(/*
           initialCameraPosition: CameraPosition(
             target: LatLng(-1.4437121, -48.4630365),
-            zoom: 11.0,
+            zoom: 11.0,*/
+            initialCameraPosition: CameraPosition(target: initialcameraposition),
+            mapType: MapType.normal,
+            onMapCreated: _onMapCreated,
+            myLocationEnabled: true
           ),
         ),
-      ),
-    );
+      );
   }
 }
